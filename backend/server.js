ba4fda0ae943d9449cc90469e8de9e884f5ae007
madhusanka-96 <madhusankasadungama9@gmail.com>
2025-12-05 23:30:54 +0530

    Backend ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -44,4 +44,5 @@ app.post('/calculate-interest', (req, res) => {
     });
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
